6692867ab73ce3364086c57298a3b81022f5bdb5
Abhiram <PVAbhiram2003@users.noreply.huggingface.co>
2023-05-28 11:12:00 +0000

    Upload app.py
    Changes made

--- app.py
+++ app.py
@@ -4,39 +4,19 @@ import gradio as gr
 import time
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 from flores200_codes import flores_codes
+from langdetect import detect
 
 
-def load_models():
-    # build model and tokenizer
-    model_name_dict = {
-		  #'nllb-distilled-600M': 'facebook/nllb-200-distilled-600M',
-                  #'nllb-1.3B': 'facebook/nllb-200-1.3B',
-                  'nllb-distilled-1.3B': 'facebook/nllb-200-distilled-1.3B',
-                  #'nllb-3.3B': 'facebook/nllb-200-3.3B',
-                  }
-
-    model_dict = {}
-
-    for call_name, real_name in model_name_dict.items():
-        print('\tLoading model: %s' % call_name)
-        model = AutoModelForSeq2SeqLM.from_pretrained(real_name)
-        tokenizer = AutoTokenizer.from_pretrained(real_name)
-        model_dict[call_name+'_model'] = model
-        model_dict[call_name+'_tokenizer'] = tokenizer
-
-    return model_dict
+model = AutoModelForSeq2SeqLM.from_pretrained('facebook/nllb-200-distilled-600M')
+tokenizer = AutoTokenizer.from_pretrained('facebook/nllb-200-distilled-600M')
+        
 
 
 def translation(source, target, text):
-    if len(model_dict) == 2:
-        model_name = 'nllb-distilled-1.3B'
-
     start_time = time.time()
     #source = flores_codes[source]
     #target = flores_codes[target]
 
-    model = model_dict[model_name + '_model']
-    tokenizer = model_dict[model_name + '_tokenizer']
 
     translator = pipeline('translation', model=model, tokenizer=tokenizer, src_lang=source, tgt_lang=target)
     output = translator(text, max_length=400)
@@ -55,10 +35,6 @@ def translation(source, target, text):
 
 if __name__ == '__main__':
     print('\tinit models')
-
-    global model_dict
-
-    model_dict = load_models()
     
     # define gradio demo
     lang_codes = list(flores_codes.keys())
@@ -70,7 +46,7 @@ if __name__ == '__main__':
 
     outputs = gr.outputs.JSON()
 
-    title = "NLLB distilled 1.3B distilled demo"
+    title = "NLLB based API"
 
     demo_status = "Demo is running on CPU"
     description = f"Details: https://github.com/facebookresearch/fairseq/tree/nllb. {demo_status}"
